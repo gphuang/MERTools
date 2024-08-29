bfe89d50f309ae8346cfc7531f918ee962b127c1
GP <gpuhuang@gmail.com>
2024-08-29 16:37:23 +0300

    edit gitignore

--- MERBench/run-prep-data.sh
+++ MERBench/run-prep-data.sh
@@ -1,3 +1,15 @@
+#!/bin/bash
+#SBATCH --time=01:10:59
+#SBATCH --mem=250G
+#SBATCH --partition=gpu-h100-80g 
+#SBATCH --gpus=1
+#SBATCH --cpus-per-task=6
+#SBATCH --output=logs/%A.out
+#SBATCH --job-name=conv
+#SBATCH -n 1
 
+module load mamba
 
-toolkit/proprocess/iemocap.py
+source activate merbench-env
+
+python toolkit/proprocess/iemocap.py
